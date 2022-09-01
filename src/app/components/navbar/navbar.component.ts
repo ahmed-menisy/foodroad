9c516f7c9e5349d4343a1775292b62d03fe5cf04
ahmed-menisy <amelmaster16@gmail.com>
2022-09-01 16:35:24 +0200

    update-7

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -4,11 +4,12 @@ import {
   OnInit,
   Renderer2,
   ViewChild,
-  ChangeDetectorRef
+  ChangeDetectorRef,
 } from '@angular/core';
 import { AuthService } from './../../services/auth.service';
 import { Router } from '@angular/router';
 import { DataService } from 'src/app/services/data.service';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -19,47 +20,74 @@ export class NavbarComponent implements OnInit {
     private _AuthService: AuthService,
     private _Router: Router,
     private _Renderer2: Renderer2,
-    private _DataService:DataService,
-    private cdRef:ChangeDetectorRef,
-    private ref:ElementRef
+    private _DataService: DataService,
+    private cdRef: ChangeDetectorRef,
+    private ref: ElementRef
   ) {}
   @ViewChild('navRef') navRef!: ElementRef;
-  favData:any;
-  favNum!:number
+  favData: any;
+  favNum!: number;
   ngOnInit(): void {
     // when change data serveice  to get data from fav share
-    this._DataService.favDataShare.subscribe(()=>{
-      this.favData = this._DataService.favDataShare.getValue()
+    this._DataService.favDataShare.subscribe(() => {
+      this.favData = this._DataService.favDataShare.getValue();
       // console.log(this.favData,'this.favData');
-      this.favNum = this.favData ? this.favData.size : 0
-      this.cdRef.detectChanges()
-    })
-  
+      this.favNum = this.favData ? this.favData.size : 0;
+      this.cdRef.detectChanges();
+    });
+
     // change nav bar fixed
     this._Renderer2.listen(window, 'scroll', () => {
       if (scrollY > 200) {
         this._Renderer2.addClass(this.navRef.nativeElement, 'fixed-top');
         this._Renderer2.addClass(this.navRef.nativeElement, 'bg-white');
-      }else {
+      } else {
         this._Renderer2.removeClass(this.navRef.nativeElement, 'fixed-top');
         this._Renderer2.removeClass(this.navRef.nativeElement, 'bg-white');
       }
     });
   }
-  // log out function 
+  // log out function
   logOut(): void {
+    //---------- sweat alert
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 2000,
+      timerProgressBar: true,
+    });
+    Toast.fire({
+      icon: 'warning',
+      title: `
+     Wait
+      `,
+    });
     this._AuthService.logOut().subscribe({
       next: (response) => {
         if (response.message === 'success') {
+          Toast.fire({
+            icon: 'success',
+            title: `
+            You Will Miss Us
+            `,
+          });
           this._AuthService.userData.next(null);
           localStorage.removeItem('fuser');
           this._Router.navigate(['/login']);
+        } else {
+          Toast.fire({
+            icon: 'error',
+            title: `
+            ${response.message}
+            `,
+          });
         }
       },
     });
   }
   // to show cart
-  show():void {
-    this.ref.nativeElement.querySelector('.cart-fav').classList.toggle('show')
+  show(): void {
+    this.ref.nativeElement.querySelector('.cart-fav').classList.toggle('show');
   }
 }
